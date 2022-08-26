a44c7a05c6649749be4ac4276141fdc93afe671f
charles busso <109078429+charlesbusso@users.noreply.github.com>
2022-08-26 10:51:25 -0300

    novo

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const txtName = document.getElementById(txtName)
+const txtEmail = document.getElementById(txtEmail)
+const txtLevel = document.getElementById(txtLevel)
+const txtCharacter = document.getElementById(txtCharacter)
+const btnSubscribe = document.getElementById(btnSusbribe)
+
+btnSubscribe.addEventListener('click', () => {
+    
+})
+
